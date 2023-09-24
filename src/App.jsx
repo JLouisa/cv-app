275c66214f25ccc89b32aa299a184d1573980a33
Jlouisa <jonathan.louisa@gmail.com>
2023-09-24 11:44:30 +0200

    Add Personalia form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-// import { useState } from "react";
+import { useState } from "react";
 import InfoSection from "./components/InfoSection.jsx";
 import DisplaySection from "./components/DisplaySection.jsx";
 import "./styles/App.css";
 
 function App() {
+  const [personInfo, setPersonInfo] = useState("");
+
+  const addPersonInfo = (info) => {
+    console.log(info);
+  };
+
   return (
     <>
       <header>
         <h1>CV Builder</h1>
       </header>
       <main>
-        <InfoSection />
+        <InfoSection addPersonInfo={addPersonInfo} />
         <DisplaySection />
       </main>
     </>
--- src/components/Personalia.jsx
+++ src/components/Personalia.jsx
@@ -4,8 +4,18 @@ import Button from "./Button.jsx";
 function Personalia() {
   const [show, setShow] = useState(false);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNum, setphoneNum] = useState("");
+
   function onHandlerBtn() {
     setShow(!show);
+  }
+
+  function personaliaSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
   }
 
   return (
@@ -18,25 +28,65 @@ function Personalia() {
         <form>
           <label htmlFor="fName">
             {"First Name "}
-            <input type="text" placeholder="First Name" id="fName" autoComplete="given-name" />
+            <input
+              type="text"
+              placeholder="First Name"
+              id="fName"
+              autoComplete="given-name"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </label>
           <div>
             <label htmlFor="lName">
               {"Last Name "}
-              <input type="text" placeholder="Last Name" id="lName" autoComplete="family-name" />
+              <input
+                type="text"
+                placeholder="Last Name"
+                id="lName"
+                autoComplete="family-name"
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="eMail">
               {"E-mail address "}
-              <input type="email" placeholder="john@doe.nl" id="eMail" autoComplete="off" />
+              <input
+                type="email"
+                placeholder="john@doe.nl"
+                id="eMail"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="phone">
               {"Phone Number "}
-              <input type="number" placeholder="+316 555 5555" id="phone" autoComplete="phone" />
+              <input
+                type="number"
+                placeholder="+316 555 5555"
+                id="phone"
+                autoComplete="phone"
+                onClick={personaliaSubmit}
+                value={phoneNum}
+                onChange={(e) => {
+                  setphoneNum(e.target.value);
+                }}
+              />
             </label>
+          </div>
+          <div>
+            <input type="submit" id="personaliaSubmit" />
           </div>
         </form>
       )}
